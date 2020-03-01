f43ef05d53d0ff720fcbcf0d9615f1d8b5c55563
vivin prasannaa <vivinprasannaa@outlook.com>
2020-03-02 00:08:40 +0530

    Modified Contracts by making post count public

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -1,29 +1,28 @@
 pragma solidity >=0.4.22 <0.7.0;
 
 contract SocialNetwork {
-    
-     string public name;
-      mapping(uint => Post) public Posts;
-      uint postCount = 0;
-      struct Post {
-          uint id;
-          string postContent;
-          address authour;
-          uint tipAmount;
-          
-      }
-    
-    event postCreated(  uint id,
-          string postContent,
-          address authour,
-          uint tipAmount);
-    
-     constructor() public{
-     }
-     
-     function createPost(string memory  _postContent) public {
-         postCount++;
-         Posts[postCount] = Post(postCount,_postContent,msg.sender,0);
-         emit postCreated(postCount,_postContent,msg.sender,0);
-     }
+    string public name;
+    mapping(uint256 => Post) public Posts;
+    uint256 public postCount = 0;
+    struct Post {
+        uint256 id;
+        string postContent;
+        address authour;
+        uint256 tipAmount;
+    }
+
+    event postCreated(
+        uint256 id,
+        string postContent,
+        address authour,
+        uint256 tipAmount
+    );
+
+    constructor() public {}
+
+    function createPost(string memory _postContent) public {
+        postCount++;
+        Posts[postCount] = Post(postCount, _postContent, msg.sender, 0);
+        emit postCreated(postCount, _postContent, msg.sender, 0);
+    }
 }
